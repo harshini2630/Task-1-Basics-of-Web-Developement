3dc7b09cdc1a0a1a92032b76af6684f663cc5f80
harshini2630 <chokkarapuharshini@gmail.com>
2025-05-20 21:08:17 +0530

    Modified

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 function showMessage() {
     alert("Hello! You clicked the button.");
-=======
-function showMessage() {
-    alert("Hello! You clicked the button.");
->>>>>>> 7f03e7020af5ba3c7fa6441cd34944cead18b66f
-  }
+}
